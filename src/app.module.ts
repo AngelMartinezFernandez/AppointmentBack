18815efede1cb0c358486caf7571f8bf5193f21d
Abraham <nencho_1@hotmail.com>
2021-04-18 09:12:24 +0200

    deleted service and app cliente on appmodules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ClientsController } from './clients/clients.controller';
-import { ClientsService } from './clients/clients.service';
 import { ClientsModule } from './clients/clients.module';
 import {MongooseModule} from '@nestjs/mongoose';
 
 @Module({
   imports: [ClientsModule, MongooseModule.forRoot('mongodb://localhost/appointmentdb')],
-  controllers: [AppController, ClientsController],
-  providers: [AppService, ClientsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
+
 export class AppModule {}
